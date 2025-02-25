57d250d82e4ee5d758191f7db0ca094512d17efe
belgianwaves <belgianwaves@gmail.com>
2025-02-25 09:10:55 +0100

    ios global configuration

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -4,6 +4,15 @@ import shared
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      let config = Config()
+#if DEBUG
+      config.VERBOSE = true
+#else
+      config.VERBOSE = false
+#endif
+      config.LANGUAGE = Platform_iosKt.getCurrentLocale()
+      
+      DebugLog().verbose = config.VERBOSE
       Graph().doInit(driverFactory: DriverFactory())
     return true
   }
